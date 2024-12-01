90d99408de07576b2e72acbb8fd3aa3586aacd90
sugappa55 <kurubasugappa@gmail.com>
2024-12-01 13:25:22 +0530

    test

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
     <div className=' w-screen h-screen flex justify-center items-center'>
       {/* <DebouncedInput /> */}
       {/* <PreviousValue /> */}
-      <FetchData />
+      {/* <FetchData /> */}
     </div>
   );
 }
